83d3c7026bb7e5ae32ae1c96c056a4c310be1a5d
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-05-29 08:59:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-hello
+status = "";
+
+function setup() {
+    Canvas = createCanvas(480, 340);
+    Canvas.center();
+    Video = createCapture(VIDEO);
+    Video.size(480, 380);
+    Video.hide();
+}
+
+
+function Start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status:Detecting Objects";
+    Text_Box= document.getElementById("text").value;
+}
+function modelLoaded(){
+    console.log("model is Loaded");
+    status = true;
+}
+function draw(){
+    image(Video,0,0,480,380);
+}
