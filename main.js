8edaf7293ebb3a0c2e7eec83a3916755f11e4e4d
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-06-02 09:24:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 status = "";
-
+objects= [];
 function setup() {
     Canvas = createCanvas(480, 340);
     Canvas.center();
@@ -18,6 +18,39 @@ function modelLoaded(){
     console.log("model is Loaded");
     status = true;
 }
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
 function draw(){
     image(Video,0,0,480,380);
-}
+    if (status != "") {
+        objectDetector.detect(Video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status:Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+           if(objects[i].label == Text_Box){
+           Video.stop();
+           objectDetector.detect(gotResult);
+           document.getElementById("name").innerHTML = Text_Box +" Found";
+           var synth = window.speechSynthesis;
+           var utterThis = new SpeechSynthesisUtterance(Text_Box.value+" Found");
+           }
+           if(objects[i].label != Text_Box){
+            Video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("name").innerHTML = Text_Box +"  Not Found";
+            var synth = window.speechSynthesis;
+            var utterThis = new SpeechSynthesisUtterance(Text_Box.value+" Not Found");
+           }
+            }
+        }
+    }
